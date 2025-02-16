1ef41ed57e3b43d6c86d28ea35f2b62b5e351ae1
braverokmc79 <braverokmc79@gmail.com>
2025-02-16 17:02:05 +0900

    23. 카드 만들기 상세 구현 && 24. Shadcn UI 버튼을 확장하는 방법

--- src/app/dashboard/components/employees-stats.tsx
+++ src/app/dashboard/components/employees-stats.tsx
@@ -1,12 +1,40 @@
-import { Card } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { UserIcon } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react'
 
 const EmployeesStats:React.FC = () => {
   return (
     <div className='grid lg:grid-cols-3 gap-4'>
-        <Card>card 1 </Card>
-        <Card>card 2 </Card>
-        <Card>card 3 </Card>
+        <Card>
+            <CardHeader>
+                <CardTitle className='text-base'>전체 직원</CardTitle>    
+            </CardHeader>   
+            <CardContent className='flex justify-between items-center'>
+                <div className='flex gap-2'>
+                    <UserIcon />
+                </div>
+                <div className='text-5xl font-bold'>
+                    100
+                </div>
+                <div>
+                    <Button size="xs"  asChild>
+                        <Link href="/dashboard/employees">전체 보기</Link>
+                    </Button>
+                </div>
+            </CardContent> 
+        </Card>
+        <Card>
+            <CardHeader>
+                <CardTitle className='text-base'>전체 직원 </CardTitle>    
+            </CardHeader>    
+        </Card>
+        <Card className='border-pink-500'>
+            <CardHeader>
+                <CardTitle className='text-base'> 전체 직원</CardTitle> 
+            </CardHeader>    
+        </Card>
     </div>
   )
 }
