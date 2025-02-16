f95b4288437dda012f1179533ff2047d872a8745
braverokmc79 <braverokmc79@gmail.com>
2025-02-16 17:31:16 +0900

    25. 카드 만들기 상세 구현 2

--- src/app/dashboard/components/employees-stats.tsx
+++ src/app/dashboard/components/employees-stats.tsx
@@ -1,14 +1,18 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { UserIcon } from 'lucide-react';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import {  AlertTriangleIcon, BadgeCheckIcon, UserCheck2Icon, UserIcon, UserRoundIcon } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react'
 
 const EmployeesStats:React.FC = () => {
+  const totalEmployees = 100;
+  const employeesPresent = 85;
+  const employeesPresentPercentage = (employeesPresent / totalEmployees) * 100;
+
   return (
     <div className='grid lg:grid-cols-3 gap-4'>
         <Card>
-            <CardHeader>
+            <CardHeader className='pb-2'>
                 <CardTitle className='text-base'>전체 직원</CardTitle>    
             </CardHeader>   
             <CardContent className='flex justify-between items-center'>
@@ -16,7 +20,7 @@ const EmployeesStats:React.FC = () => {
                     <UserIcon />
                 </div>
                 <div className='text-5xl font-bold'>
-                    100
+                    {totalEmployees}
                 </div>
                 <div>
                     <Button size="xs"  asChild>
@@ -25,14 +29,37 @@ const EmployeesStats:React.FC = () => {
                 </div>
             </CardContent> 
         </Card>
+
+
         <Card>
-            <CardHeader>
-                <CardTitle className='text-base'>전체 직원 </CardTitle>    
-            </CardHeader>    
+            <CardHeader className='pb-2'>
+                <CardTitle className='text-base'>직원 출근율</CardTitle>    
+            </CardHeader>   
+            <CardContent className='flex justify-between items-center'>
+                <div className='flex gap-2'>
+                    {employeesPresentPercentage > 75 ? <UserCheck2Icon /> : <UserRoundIcon />}
+                    <div className='text-5xl font-bold'>{employeesPresent}</div>                    
+                </div>                                
+            </CardContent> 
+            <CardFooter>
+            {employeesPresentPercentage > 75 ?(
+                <span className='text-xs text-green-500 flex gap-1 items-center'>
+                    <BadgeCheckIcon />
+                    직원의 {employeesPresentPercentage}% 가 현재 근무 중
+                </span>
+            ):(
+                <span className='text-xs text-red-500 flex gap-1 items-center'>
+                <AlertTriangleIcon />
+                직원의 {employeesPresentPercentage}% 만 현재 근무 중
+                </span>
+            )}
+            </CardFooter>
         </Card>
+
+
         <Card className='border-pink-500'>
-            <CardHeader>
-                <CardTitle className='text-base'> 전체 직원</CardTitle> 
+            <CardHeader className='pb-2'>
+                <CardTitle className='text-base'> 이달의 직원</CardTitle> 
             </CardHeader>    
         </Card>
     </div>
